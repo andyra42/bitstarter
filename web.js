a451b5070a3f9e4cf88ed23b78337645d6de9b62
andyra42 <anand.rapolu@gmail.com>
2013-07-06 11:38:48 +0000

    Updated web.js again

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var content =fs.readFileSync("index.html");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(content);
+  response.send(content.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
